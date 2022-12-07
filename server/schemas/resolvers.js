8f1296512af24e67f240db2806a0d6c8eb85e845
sdanielewicz <sdanielewicz@gmail.com>
2022-12-06 18:49:59 -0800

    resolver work

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,10 +3,10 @@ const {Book, User} = require('../models');
 const { signToken } = require('../utils/auth');
 
 
-const resolvers {
+const resolvers = {
 Query: {
-    me:
-        async (parent, args, context) => {
+    me: 
+    async (parent, args, context) => {
         if (context.user) {
           return User.findOne({ _id: context.user._id });
         }
@@ -15,7 +15,22 @@ Query: {
 },
 
 Mutation: {
-// login:
+login:
+async (parent, {email, password }) => {
+  const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+  if (!user) {
+    throw new AuthenticationError("Wrong Username");
+  }
+
+  const correctPw = await user.isCorrectPassword(body.password);
+
+  if (!correctPw) {
+    throw new AuthenticationError("Wrong Password");
+  }
+  const token = signToken(user);
+  return { token, user };
+},
+
 addUser:
 async (parent, { name, email, password }) => {
     const user = await User.create({ name, email, password });
@@ -38,23 +53,22 @@ async (parent, args, context) => {
     throw new AuthenticationError({ message: "Couldn't find user with this id!" });
       
 },
+
 saveBook:
-
-async saveBook(parent, args, context) {
-    console.log(user);
-    if(context.user){
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $addToSet: { savedBooks: body } },
-        { new: true, runValidators: true }
-      );
-      return res.json(updatedUser);
-    }
-      throw new AuthenticationError('You need to be logged in!');
-    }
+async (parent, args, context) => {
+  if(context.user){
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      { $addToSet: { savedBooks: body } },
+      { new: true, runValidators: true }
+    );
+    return updatedUser;
   }
+  throw new AuthenticationError('You need to be logged in!');
 
 
+},
+},
 };
 
 module.exports = resolvers;
